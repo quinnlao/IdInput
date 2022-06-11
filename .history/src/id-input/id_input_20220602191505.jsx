bc57c7e048face831d45ff6997f21142b63e594a
user.email <quinnlao13@gmail.com>
2022-06-12 01:28:54 +0800

    ''

---
+++ .history/src/id-input/id_input_20220602191505.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import IdInputItem from './id_input_item/id_input_item'
+import './id_input.css'
+
+
+function IdInput() {
+    const [value1, takeNumber1] = useState('0000')
+    const [value2, takeNumber2] = useState('0000')
+    const [value3, takeNumber3] = useState('0000')
+    const [value4, takeNumber4] = useState('0000')
+    const getValue1 = (param) => {
+        takeNumber1(param)
+        console.log('value1', param)
+    }
+    const getValue2 = (param) => {
+        takeNumber2(param)
+        console.log('value1', param)
+    }
+    const getValue3 = (param) => {
+        takeNumber3(param)
+        console.log('value1', param)
+    }
+    const getValue4 = (param) => {
+        takeNumber4(param)
+        console.log('value1', param)
+    }
+    // const value = [...value1, ...value2, ...value3, ...value4]
+    // console.log(value.replace(/-/g, '').replace(/(\d{4})/g, '$1-').replace(/-*$/, ''))
+    return (
+        <div className="id_input">
+            <h4>您输入的卡号为：{value1}-{value2}-{value3}-{value4}</h4>
+            <div className="id_input_item"><IdInputItem getNumber={getValue1} /></div>
+            <div className="id_input_item"><IdInputItem getNumber={getValue2} /></div>
+            <div className="id_input_item"><IdInputItem getNumber={getValue3} /></div>
+            <div className="id_input_item"><IdInputItem getNumber={getValue4} /></div>
+        </div>
+    );
+}
+
+export default IdInput;
---
+++ .history/src/id-input/id_input_item/id_input_item_20220602191655.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import './id-input_item.css';
+
+function IdInputItem(props) {
+    const [number, setNumber] = useState('0000');//number用于存储正确格式的卡号
+    const [color, setColor] = useState('black');//定义输入框outline颜色的变量
+    const { getNumber } = props;
+    const test1 = (e) => {
+        const { value } = e.target;//获取input内容
+        console.log('当前输入：', value);
+        const reg = /^[1-9A-F][][][]+$/;
+        // const reg = /^[0-9A-F]+$/;
+        //正则：必须包含数字或字母ABCDEF
+        if (reg.test(value)) {
+            if (value.length === 4) {
+                setNumber(value);
+                getNumber(value);
+            } else {
+                if (value.length > 4) {
+                    setColor('red');//改变输入框边框颜色———红
+                    console.log('所输内容已超出长度限制')
+                } else {
+                    setColor('black');//维持输入框原来边框颜色
+                }
+            }
+        }
+        else {
+            setColor('red');//改变输入框边框颜色———红
+            console.log('所输内容不符合要求')
+        };
+    }
+    return (
+        <div>
+            <input
+                onChange={test1}
+                placeholder={number}//
+                style={{ outlineColor: color }}
+            />
+        </div>
+    );
+}
+
+export default IdInputItem
